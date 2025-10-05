553413d787fe8384e302be63f251c846045fe963
Jonathan <80174486+Almost-Awake@users.noreply.github.com>
2025-10-05 19:50:14 -0400

    create base teleop Opmode

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/Teleop.java
@@ -0,0 +1,23 @@
+package org.firstinspires.ftc.teamcode.opmodes;
+
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import dev.nextftc.ftc.NextFTCOpMode;
+
+@TeleOp
+public class Teleop extends NextFTCOpMode {
+    {
+        addComponents(/* vararg components */);
+    }
+
+    @Override
+    public void onInit() {}
+    @Override
+    public void onWaitForStart() {}
+    @Override
+    public void onStartButtonPressed() {}
+    @Override
+    public void onUpdate() {}
+    @Override
+    public void onStop() {}
+}
