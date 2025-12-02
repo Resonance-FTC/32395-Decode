f28e81c94f7ca406c2b45a7c97c8b3bf22151848
Jonathan <80174486+Almost-Awake@users.noreply.github.com>
2025-12-02 14:26:46 -0800

    Create Drivetrain.java and use it to create simplified teleop with motion and turret.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opModes/Teleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opModes/Teleop.java
@@ -1,9 +1,37 @@
 package org.firstinspires.ftc.teamcode.opModes;
 
+import static dev.frozenmilk.dairy.mercurial.continuations.Continuations.*;
+
+import com.pedropathing.follower.Follower;
+import com.pedropathing.geometry.Pose;
+
+import org.firstinspires.ftc.teamcode.subsystems.Drivetrain;
+import org.firstinspires.ftc.teamcode.subsystems.Turret;
+import org.firstinspires.ftc.teamcode.util.Constants;
+
 import dev.frozenmilk.dairy.mercurial.ftc.Mercurial;
 
+@SuppressWarnings("unused")
 public class Teleop {
+    private static Pose startPose = new Pose(18.608, 119.523, Math.toRadians(-35));
+
     public static final Mercurial.RegisterableProgram myFirstMercurialTeleOp = Mercurial.teleop(ctx -> {
 
+        Drivetrain drivetrain = new Drivetrain(ctx.hardwareMap(), ctx.gamepad1(), startPose);
+        Turret turret = new Turret(ctx.hardwareMap(), Constants.AllianceColors.BLUE, drivetrain.getFollower());
+
+        ctx.schedule(
+                sequence(
+                        waitUntil(ctx::inLoop),
+                        exec(()-> drivetrain.getFollower().startTeleopDrive()),
+                        parallel(
+                                loop(
+                                        turret.targetLockClosure
+                                )
+                        )
+
+                )
+        );
+        ctx.dropToScheduler();
     });
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Drivetrain.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Drivetrain.java
@@ -1,10 +1,11 @@
 package org.firstinspires.ftc.teamcode.subsystems;
 
+import com.pedropathing.follower.Follower;
+import com.pedropathing.geometry.Pose;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.Gamepad;
 import com.qualcomm.robotcore.hardware.HardwareMap;
-
 import org.firstinspires.ftc.teamcode.util.Constants;
 import org.firstinspires.ftc.teamcode.util.Constants.DriveConstants;
 
@@ -13,37 +14,17 @@ import dev.frozenmilk.dairy.mercurial.continuations.Closure;
 import dev.frozenmilk.dairy.mercurial.continuations.Continuations;
 
 public class Drivetrain {
-    Gamepad gamepad;
-    private DcMotorEx fL;
-    private DcMotorEx fR;
-    private DcMotorEx bL;
-    private DcMotorEx bR;
 
-    public Drivetrain(HardwareMap hardwareMap, Gamepad gamepad) {
-        this.gamepad = gamepad;
 
-        this.fL = hardwareMap.get(DcMotorEx.class, Constants.DriveConstants.FLMotorID);
-        this.fR = hardwareMap.get(DcMotorEx.class, Constants.DriveConstants.FRMotorID);
-        this.bL = hardwareMap.get(DcMotorEx.class, Constants.DriveConstants.BLMotorID);
-        this.bR = hardwareMap.get(DcMotorEx.class, Constants.DriveConstants.BRMotorID);
 
-        bR.setDirection(DcMotorSimple.Direction.REVERSE);
+    public Follower follower;
+    public Drivetrain(HardwareMap hardwareMap, Gamepad gamepad, Pose startPose) {
 
+        follower = org.firstinspires.ftc.teamcode.pedroPathing.Constants.createFollower(hardwareMap);
+        follower.setStartingPose(startPose);
+        follower.update();
     }
-    Closure driveClosure = Continuations.exec(() -> {
-        double y = -gamepad.left_stick_y;
-        double x = gamepad.left_stick_x;
-        double h = -gamepad.right_stick_x;
-
-        double denominator = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(h), 1.0);
-        double frontLeftPower = (y + x + h) / denominator;
-        double backLeftPower = (y - x + h) / denominator;
-        double frontRightPower = (y - x - h) / denominator;
-        double backRightPower = (y + x - h) / denominator;
-
-        fL.setPower(frontLeftPower);
-        bL.setPower(backLeftPower);
-        fR.setPower(frontRightPower);
-        bR.setPower(backRightPower);
-    });
+    public Follower getFollower() {
+        return follower;
+    }
 }
