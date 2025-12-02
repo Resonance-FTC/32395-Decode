914ef2b167fe2eaa424f3c8dd68334f930f98fb6
Jonathan <80174486+Almost-Awake@users.noreply.github.com>
2025-12-01 19:05:26 -0800

    Drive subsystem?

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Drivetrain.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Drivetrain.java
@@ -1,79 +1,49 @@
 package org.firstinspires.ftc.teamcode.subsystems;
 
 import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.Gamepad;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
+import org.firstinspires.ftc.teamcode.util.Constants;
 import org.firstinspires.ftc.teamcode.util.Constants.DriveConstants;
 
 import dev.frozenmilk.dairy.mercurial.continuations.Actors.Actor;
+import dev.frozenmilk.dairy.mercurial.continuations.Closure;
 import dev.frozenmilk.dairy.mercurial.continuations.Continuations;
 
 public class Drivetrain {
-    interface state {};
-    interface action {};
-
-    class Stopped implements state {};
-    class Driving implements state {};
-    class Drive implements action {};
-    class Stop implements action {};
     Gamepad gamepad;
     private DcMotorEx fL;
     private DcMotorEx fR;
     private DcMotorEx bL;
     private DcMotorEx bR;
-    public Drivetrain(HardwareMap hardwareMap, Gamepad gamepad) {
 
+    public Drivetrain(HardwareMap hardwareMap, Gamepad gamepad) {
         this.gamepad = gamepad;
 
-        fL = hardwareMap.get(DcMotorEx.class, DriveConstants.FLMotorID);
-        fR = hardwareMap.get(DcMotorEx.class, DriveConstants.FRMotorID);
-        bL = hardwareMap.get(DcMotorEx.class, DriveConstants.BLMotorID);
-        bR = hardwareMap.get(DcMotorEx.class, DriveConstants.BRMotorID);
+        this.fL = hardwareMap.get(DcMotorEx.class, Constants.DriveConstants.FLMotorID);
+        this.fR = hardwareMap.get(DcMotorEx.class, Constants.DriveConstants.FRMotorID);
+        this.bL = hardwareMap.get(DcMotorEx.class, Constants.DriveConstants.BLMotorID);
+        this.bR = hardwareMap.get(DcMotorEx.class, Constants.DriveConstants.BRMotorID);
+
+        bR.setDirection(DcMotorSimple.Direction.REVERSE);
 
     }
+    Closure driveClosure = Continuations.exec(() -> {
+        double y = -gamepad.left_stick_y;
+        double x = gamepad.left_stick_x;
+        double h = -gamepad.right_stick_x;
 
-    Actor<state, action> drive = new Actor<state, action>(
-            "Drivetrain Actor",
-            Stopped::new,
-            (currentState, receivedAction) -> {
-                    if (receivedAction instanceof Drive) {
-                        return new Driving();
-                    }
-                    else if (receivedAction instanceof Stop) {
-                        return new Stopped();
-                    }
-                    else {
-                        return currentState;
-                    }
-            },
-            (stateRegister) -> {
-                return Continuations.exec(()-> {
-                    state stateValue = stateRegister.get();
-                    if (stateValue instanceof Driving) {
-                        double y = -gamepad.left_stick_y;
-                        double x = gamepad.left_stick_x;
-                        double h = -gamepad.right_stick_x;
+        double denominator = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(h), 1.0);
+        double frontLeftPower = (y + x + h) / denominator;
+        double backLeftPower = (y - x + h) / denominator;
+        double frontRightPower = (y - x - h) / denominator;
+        double backRightPower = (y + x - h) / denominator;
 
-                        double denominator = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(h), 1.0);
-                        double frontLeftPower  = (y + x + h) / denominator;
-                        double backLeftPower   = (y - x + h) / denominator;
-                        double frontRightPower = (y - x - h) / denominator;
-                        double backRightPower  = (y + x - h) / denominator;
-
-                        fL.setPower(frontLeftPower);
-                        bL.setPower(backLeftPower);
-                        fR.setPower(frontRightPower);
-                        bR.setPower(backRightPower);
-                    } else if (stateValue instanceof Stopped) {
-                        fL.setPower(0);
-                        fR.setPower(0);
-                        bL.setPower(0);
-                        bR.setPower(0);
-                    }
-                }
-                );
-            }
-    );
+        fL.setPower(frontLeftPower);
+        bL.setPower(backLeftPower);
+        fR.setPower(frontRightPower);
+        bR.setPower(backRightPower);
+    });
 }
-
