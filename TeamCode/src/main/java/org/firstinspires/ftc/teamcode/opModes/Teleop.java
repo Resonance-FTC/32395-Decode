390df25db9ba782ece6541cc46d205390f4d277b
Jonathan <80174486+Almost-Awake@users.noreply.github.com>
2025-12-02 16:24:50 -0800

    Shmaybe this works?

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opModes/Teleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opModes/Teleop.java
@@ -3,27 +3,41 @@ package org.firstinspires.ftc.teamcode.opModes;
 import static dev.frozenmilk.dairy.mercurial.continuations.Continuations.*;
 
 import com.pedropathing.follower.Follower;
+import com.pedropathing.geometry.BezierLine;
 import com.pedropathing.geometry.Pose;
+import com.pedropathing.paths.HeadingInterpolator;
+import com.pedropathing.paths.Path;
+import com.pedropathing.paths.PathChain;
 
 import org.firstinspires.ftc.teamcode.subsystems.Drivetrain;
 import org.firstinspires.ftc.teamcode.subsystems.Turret;
 import org.firstinspires.ftc.teamcode.util.Constants;
 
+import java.util.function.Supplier;
+
 import dev.frozenmilk.dairy.mercurial.ftc.Mercurial;
 
 @SuppressWarnings("unused")
 public class Teleop {
-    private static Pose startPose = new Pose(18.608, 119.523, Math.toRadians(-35));
-
+    private static final Pose startPose = new Pose(18.608, 119.523, Math.toRadians(-35));
+    
     public static final Mercurial.RegisterableProgram myFirstMercurialTeleOp = Mercurial.teleop(ctx -> {
 
         Drivetrain drivetrain = new Drivetrain(ctx.hardwareMap(), ctx.gamepad1(), startPose);
-        Turret turret = new Turret(ctx.hardwareMap(), Constants.AllianceColors.BLUE, drivetrain.getFollower());
+        Follower follower = drivetrain.follower;
+        Turret turret = new Turret(ctx.hardwareMap(), Constants.AllianceColors.BLUE, follower);
 
+        Supplier<PathChain> pathChain = () -> follower.pathBuilder() //Lazy Curve Generation
+                .addPath(new Path(new BezierLine(follower::getPose, new Pose(30, 0))))
+                .setHeadingInterpolation(HeadingInterpolator.linearFromPoint(follower::getHeading, Math.toRadians(45), 0.8))
+                .build();
+        
         ctx.schedule(
                 sequence(
                         waitUntil(ctx::inLoop),
-                        exec(()-> drivetrain.getFollower().startTeleopDrive()),
+                        exec(()-> {
+                            follower.startTeleopDrive();
+                        }),
                         parallel(
                                 loop(
                                         turret.targetLockClosure
@@ -32,6 +46,19 @@ public class Teleop {
 
                 )
         );
+
+        ctx.bindExec(() -> ctx.gamepad1().aWasPressed(),
+                drivetrain.followPath(pathChain)
+        );
+
+        ctx.bindSpawn(ctx.risingEdge( ()-> ctx.gamepad1().right_bumper),
+                exec(()->drivetrain.setSpeed(.2))
+        );
+
+        ctx.bindSpawn(ctx.risingEdge( ()-> !ctx.gamepad1().right_bumper),
+                exec(()->drivetrain.setSpeed(1))
+        );
+
         ctx.dropToScheduler();
     });
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Drivetrain.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Drivetrain.java
@@ -1,7 +1,10 @@
 package org.firstinspires.ftc.teamcode.subsystems;
 
+import static dev.frozenmilk.dairy.mercurial.continuations.Continuations.command;
+
 import com.pedropathing.follower.Follower;
 import com.pedropathing.geometry.Pose;
+import com.pedropathing.paths.PathChain;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.Gamepad;
@@ -9,20 +12,47 @@ import com.qualcomm.robotcore.hardware.HardwareMap;
 import org.firstinspires.ftc.teamcode.util.Constants;
 import org.firstinspires.ftc.teamcode.util.Constants.DriveConstants;
 
+import java.util.function.Supplier;
+
 import dev.frozenmilk.dairy.mercurial.continuations.Actors.Actor;
 import dev.frozenmilk.dairy.mercurial.continuations.Closure;
-import dev.frozenmilk.dairy.mercurial.continuations.Continuations;
+import dev.frozenmilk.dairy.mercurial.continuations.Continuations.Command;
 
 public class Drivetrain {
 
-
-
+    private double speed = 1.0;
+    private Gamepad gamepad;
     public Follower follower;
     public Drivetrain(HardwareMap hardwareMap, Gamepad gamepad, Pose startPose) {
+        this.gamepad = gamepad;
 
         follower = org.firstinspires.ftc.teamcode.pedroPathing.Constants.createFollower(hardwareMap);
         follower.setStartingPose(startPose);
         follower.update();
+    }
+    public Command followPath(Supplier<PathChain> pathChainSupplier){
+        return command()
+                .setInit(()-> {
+                    follower.breakFollowing();
+                    follower.followPath(pathChainSupplier.get());
+                })
+                .setFinished(() -> !follower.isBusy() || gamepad.bWasPressed());
+    }
+    public void drive(){
+        follower.startTeleopDrive();
+        follower.setTeleOpDrive(
+                -gamepad.left_stick_y * speed,
+                -gamepad.left_stick_x * speed,
+                -gamepad.right_stick_x * speed,
+                true);
+    }
+    public void setSpeed(double speed){
+        this.speed = speed;
+        follower.setTeleOpDrive(
+                -gamepad.left_stick_y * speed,
+                -gamepad.left_stick_x * speed,
+                -gamepad.right_stick_x * speed,
+                true);
     }
     public Follower getFollower() {
         return follower;
